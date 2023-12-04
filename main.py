03f395a107565c6474988ec7abd74ae415b9448e
anaalumu <100475297@alumnos.uc3m.es>
2023-12-04 18:34:03 +0100

    clase, creadas las files

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ bp = Blueprint("main", __name__)
 
 @bp.route("/")
 def index():
-    return render_template("homePage/template.html")
+    return render_template("homePage.html")
 
 @bp.route("/recipes")
+def index():
+    return render_template("recipe.html")
+
+@bp.route("/userregister")
+def index():
+    return render_template("user.html")
+
